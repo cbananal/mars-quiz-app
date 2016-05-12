29e55a6feec13563b5bbf339f1a87b5f1e30c3d2
Claudine <claudine.bananal@gmail.com>
2016-05-12 06:15:08 +0000

    initialize

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
+require ('./main.scss');
+
+
+//Parent Component
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+} //end class App
+
+
+
+ReactDOM.render(<App />, document.getElementById('mars-react'));
