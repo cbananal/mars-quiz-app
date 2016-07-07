fa32bffc87203069f9fb8b8ac6aa4fcc13fae026
Claudine <claudine.bananal@gmail.com>
2016-07-07 22:19:37 +0000

    enable navigation back to start when on the fail page

--- src/components/failpage.jsx
+++ src/components/failpage.jsx
@@ -1,6 +1,30 @@
 import * as React from 'react';
 
 export default class FailPage extends React.Component {
+
+  //Navigates back to beginning of quiz
+  _nextPage() {
+    this.props.navigate('start');
+  }
+
+  //Starts timer
+  componentDidMount() {
+    let time = 0;
+
+    this.timer = setInterval(()=> {
+      time ++;
+
+      //Go back to start page after five counts for another try
+      if (time === 5) {
+        this._nextPage();
+      }
+    },1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
   render() {
     return(
       <div className="fail-page">
